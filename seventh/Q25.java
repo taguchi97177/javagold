89693cd949869f05509663827ef18f67e30a6194
taguchi97177 <taguchi@esm.co.jp>
2018-06-29 18:19:37 +0900

    テスト

--- seventh/Q25.java
+++ seventh/Q25.java
@@ -3,12 +3,16 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.nio.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.DosFileAttributeView;
+import java.nio.file.attribute.DosFileAttributes;
 
 public class Q25 {
     public static void main(String[] aaa){
         Path path1 = Paths.get("test.txt");
         try{
-            BasicFileAttributes attr = Files.readAttributes(path1, BasicFileAttributes.class);
+            DosFileAttributes attr = Files.readAttributes(path1, DosFileAttributes.class);
+            Files.getFileAttributeView(path1, DosFileAttributeView.class);
+
             System.out.println("create time::"+attr.creationTime());
             System.out.println("key::"+attr.fileKey());
             System.out.println("isDir::"+attr.isDirectory());
